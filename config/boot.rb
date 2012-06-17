bb4c9124a9a45f5fae7e71f711ea7a905e07d031
Iwakura Taro <taro@mail333.com>
2012-06-17 19:05:02 +0300

    Try to fix encoding issue.

--- config/boot.rb
+++ config/boot.rb
@@ -2,15 +2,15 @@
 PADRINO_ENV  = ENV['PADRINO_ENV'] ||= ENV['RACK_ENV'] ||= 'development'  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
-# Load our dependencies
-require 'rubygems' unless defined?(Gem)
-require 'bundler/setup'
-Bundler.require(:default, PADRINO_ENV)
-
 if RUBY_VERSION > '1.9'
   Encoding.default_external = Encoding::UTF_8
   Encoding.default_internal = Encoding::UTF_8
 end
+
+# Load our dependencies
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+Bundler.require(:default, PADRINO_ENV)
 
 ##
 # Enable devel logging
