32701310cbd1e3512fe8c1d0db097b94a7faa190
Iwakura Taro <taro@mail333.com>
2012-06-18 13:17:35 +0300

    AJAX form.

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -4,7 +4,15 @@ Complimentator.controller do
     if params[:name]
       @words = Word.expand(params[:name])
     end
-    render '/home/index'
+    Padrino.logger.info( (request.methods - Object.methods).inspect)
+    Padrino.logger.info request.preferred_type
+    if request.xhr?
+      content_type 'application/javascript'
+      render '/home/index.js', :layout => false
+    else
+      render '/home/index.html'
+    end
+
   end
 
 end
