234b3739f66a44759d9790b02ab04aab8e428744
Iwakura Taro <taro@mail333.com>
2012-06-18 13:19:14 +0300

    Removing logging

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -4,8 +4,6 @@ Complimentator.controller do
     if params[:name]
       @words = Word.expand(params[:name])
     end
-    Padrino.logger.info( (request.methods - Object.methods).inspect)
-    Padrino.logger.info request.preferred_type
     if request.xhr?
       content_type 'application/javascript'
       render '/home/index.js', :layout => false
